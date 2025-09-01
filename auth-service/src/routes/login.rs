bbfc5eb43fde6ed49de8b7b128be791af1e562eb
John Simon <johncsimon@gmail.com>
2025-08-31 17:38:54 -0700

    progress so far

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,25 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, Email, Password},
+};
+
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let email =
+        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password =
+        Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+
+    Ok(StatusCode::OK.into_response())
+}
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    email: String,
+    password: String,
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,10 +1,32 @@
 use crate::helpers::TestApp;
+use auth_service::ErrorResponse;
 
 #[tokio::test]
-async fn login_should_return_200() {
+async fn should_return_422_if_malformed_credentials() {
     let app = TestApp::new().await;
+    let login_body = serde_json::json!({
+        "BADemail": "user@example.com",
+        "BADpassword": "password123"
+    });
 
-    let response = app.post_login().await;
+    let response = app.post_login(&login_body).await;
 
-    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.status().as_u16(), 422);
+}
+
+//...
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    // Call the log-in route with invalid credentials and assert that a
+    // 400 HTTP status code is returned along with the appropriate error message.
+    let app = TestApp::new().await;
+    let login_body = serde_json::json!({
+        "email": "user@example.com",
+        "password": "short"
+    });
+
+    let response = app.post_login(&login_body).await;
+
+    assert_eq!(response.status().as_u16(), 400);
 }
